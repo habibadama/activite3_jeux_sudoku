8461d60b3da77c9a41d5ddc8b2b4870c67d6d576
Habib Adama Dioumanera <habibadamadioumanera@gmail.com>
2024-05-24 17:36:45 +0100

    Gestion des erreurs apres l'analyse du jenkins 3

--- src/main/java/ElementDeGrilleImplAsChar.java
+++ src/main/java/ElementDeGrilleImplAsChar.java
@@ -1,11 +1,25 @@
-
+/**
+ * Implémentation de l'interface ElementDeGrille utilisant un caractère comme valeur.
+ */
 public class ElementDeGrilleImplAsChar implements ElementDeGrille {
-    private char value;
 
-    public ElementDeGrilleImplAsChar(char value) {
+    /**
+     * La valeur de l'élément de grille.
+     */
+    private final char value;
+
+    /**
+     * Constructeur prenant en paramètre la valeur de l'élément de grille.
+     * @param value la valeur de l'élément de grille.
+     */
+    public ElementDeGrilleImplAsChar(final char value) {
         this.value = value;
     }
 
+    /**
+     * Renvoie la valeur de l'élément de grille.
+     * @return la valeur de l'élément de grille.
+     */
     @Override
     public char getValue() {
         return value;
